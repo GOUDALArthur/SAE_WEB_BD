22e9981f25a91acd8561040670f9cf9583a067be
slim inah <selimm.hani@gmail.com>
2024-01-14 23:33:11 +0100

    Petite correction

--- appFestiut/commands.py
+++ appFestiut/commands.py
@@ -12,86 +12,66 @@ def syncdb():
     """Create all missing tables"""
     db.create_all()
 
-    # Festivalier
     user1 = Festivalier(id_fest=1, prenom_fest="Karim", nom_fest="Poulet", mail_fest="b@gmail.com", 
                         mdp_fest=sha256("b".encode()).hexdigest(), num_fest="982732837")
     db.session.add(user1)
 
-    # TypeMusique
     type1 = TypeMusique(id_type=1, type_mus="Rock")
     db.session.add(type1)
 
-    # StyleMusique
     style1 = StyleMusique(id_style=1, style="Hard Rock", id_type=1)
     db.session.add(style1)
 
-    # Groupe
     group1 = Groupe(id_gr=1, nom_gr="The Rockers", description_gr="A rock band", reseaux_gr="www.therockers.com", id_style=1)
     db.session.add(group1)
 
-    # Artiste
     artist1 = Artiste(id_art=1, nom_art="Rocky", id_gr=1)
     db.session.add(artist1)
 
-    # Instrument
     instr1 = Instrument(id_instr=1, instrument="Guitar")
     db.session.add(instr1)
 
-    # JouerInstru
     jouer1 = JouerInstru(id_art=1, id_instr=1)
     db.session.add(jouer1)
 
-    # ActiviteAnnexe
     act1 = ActiviteAnnexe(id_act_ann=1, description_act_ann="Fun activity", date_debut_act_ann=datetime.now(), 
                           date_fin_act_ann=datetime.now(), id_type_act_ann=1, id_lieu=1)
     db.session.add(act1)
 
-    # TypeActiviteAnnexe
     type_act1 = TypeActiviteAnnexe(id_type_act_ann=1, activite="Music")
     db.session.add(type_act1)
 
-    # Lieu
     lieu1 = Lieu(id_lieu=1, lieu="Concert Hall", capacite_lieu=500)
     db.session.add(lieu1)
 
-    # Billet
     billet1 = Billet(id_bil=1, duree_val_bil=3, id_proprietaire=1, date_achat_bil=datetime.now().date())
     db.session.add(billet1)
 
-    # Participer
     part1 = Participer(id_gr=1, id_act_ann=1)
     db.session.add(part1)
 
-    # FavoriserGroupe
     fav_gr1 = FavoriserGroupe(id_fest=1, id_gr=1)
     db.session.add(fav_gr1)
 
-    # FavoriserStyle
     fav_st1 = FavoriserStyle(id_fest=1, id_style=1)
     db.session.add(fav_st1)
 
-    # Photo
     photo1 = Photo(id_photo=1, id_gr=1, photo=b'\x00\x01\x02\x03')
     db.session.add(photo1)
 
-    # Hebergement
     heberg1 = Hebergement(id_heberg=1, libelle_heberg="Hotel", capacite_heberg=100)
     db.session.add(heberg1)
 
-    # Occuper
     occ1 = Occuper(id_gr=1, id_heberg=1, date_heberg=datetime.now().date())
     db.session.add(occ1)
 
-    # Concert
     concert1 = Concert(id_concert=1, date_debut_concert=datetime.now(), date_fin_concert=datetime.now(), 
                        duree_montage=2, duree_demontage=2, id_gr=1, id_lieu=1)
     db.session.add(concert1)
 
-    # Reserver
     res1 = Reserver(id_concert=1, id_bil=1)
     db.session.add(res1)
 
-    # Deplacer
     dep1 = Deplacer(id_lieu_depart=1, id_lieu_arrivee=2, temps_de_trajet=30)
     db.session.add(dep1)
 
